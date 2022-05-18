43942b996d0e4ac9b71adfa0d5b6a54caff615c7
Shim'on Sukholuski <eyesky39@gmail.com>
2022-05-18 14:04:47 +0300

    Top Panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { PostComponent } from './components/post/post.component';
+import { TopPanelComponent } from './components/top-panel/top-panel.component';
+import { TagsComponent } from './components/tags/tags.component';
+import { SidePanelComponent } from './components/side-panel/side-panel.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostComponent,
+    TopPanelComponent,
+    TagsComponent,
+    SidePanelComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
